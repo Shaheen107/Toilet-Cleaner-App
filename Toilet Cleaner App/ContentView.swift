bdb14f31660f55dc5c76574df16db6f2f5b95899
dsv <zxcv>
2024-10-01 00:40:10 +0500

    Initial Commit

---
+++ Toilet Cleaner App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Toilet Cleaner App
+//
+//  Created by Muhammad Rizwan on 01/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Toilet Cleaner App/Toilet_Cleaner_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toilet_Cleaner_AppApp.swift
+//  Toilet Cleaner App
+//
+//  Created by Muhammad Rizwan on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Toilet_Cleaner_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
